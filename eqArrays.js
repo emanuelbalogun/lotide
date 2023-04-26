d0506f1a45a21c23bb5f9d659eb022e78fe11122
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-04-26 13:45:05 -0500

    Implemented Array comparison function

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = (actual, expected) => {
+  let result =
+    actual === expected
+      ? `✅✅✅ Assertion Passed: [ ${actual}] === [ ${expected} ]`
+      : `🛑🛑🛑 Assertion failed: [ ${actual}  ] !== [ ${expected} ]`;
+  console.log(result);
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), false);
+assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), false);
