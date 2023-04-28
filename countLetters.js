391f18f9180b9d0aeced937de71a93f352dfd1c7
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-04-28 14:10:21 -0500

    Completed countLetters function: Tested and linted

--- countLetters.js
+++ countLetters.js
@@ -9,6 +9,7 @@ const assertEqual = (actual, expected) => {
 const countLetters = function (arrg) {
   let result = {};
   for (const itm of arrg) {
+    if (itm === " ") continue;
     if (result[itm]) {
       result[itm] += 1;
     } else {
@@ -19,4 +20,3 @@ const countLetters = function (arrg) {
 };
 
 console.log(countLetters("lighthouse in the house"));
-{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, ' ': 3, n: 1 }
