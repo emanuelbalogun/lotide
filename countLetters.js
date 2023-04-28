70b3d267dca2f007262431ce21f3dd32d7b2e410
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-04-28 14:05:02 -0500

    Working in progress: First working Code

--- countLetters.js
+++ countLetters.js
@@ -6,8 +6,17 @@ const assertEqual = (actual, expected) => {
   console.log(result);
 };
 
-const countLetters = function() {
-  console.log("implementing count letter");
-}
+const countLetters = function (arrg) {
+  let result = {};
+  for (const itm of arrg) {
+    if (result[itm]) {
+      result[itm] += 1;
+    } else {
+      result[itm] = 1;
+    }
+  }
+  return result;
+};
 
-countLetters();
+console.log(countLetters("lighthouse in the house"));
+{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, ' ': 3, n: 1 }
