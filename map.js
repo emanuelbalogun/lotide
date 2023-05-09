adefba308430514d0dd52d326f0adead84413d62
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-09 16:08:38 -0500

    Added test scenarios

--- map.js
+++ map.js
@@ -1,5 +1,24 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2)
+    ? `✅✅✅ Assertion Passed: [ ${arr1}] === [ ${arr2} ]`
+    : `🛑🛑🛑 Assertion failed: [ ${arr1}  ] !== [ ${arr2} ]`;
+  console.log(result);
+};
+
 const words = ["ground", "control", "to", "major", "tom"];
-const map = function (arr,callback) {
+const map = function(arr,callback) {
   const results = [];
   for (let item of arr) {
     results.push(callback(item));
@@ -9,3 +28,6 @@ const map = function (arr,callback) {
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]),['g','c','t','m','t']);
+assertArraysEqual(map(words, word => word[1]),['r','o','o','a','o']);
