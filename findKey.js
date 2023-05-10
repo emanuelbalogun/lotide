ae87dcdb39827c011228aa10279cbd64e1b5af3c
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-10 13:04:25 -0500

    Implemented the lotide findkey function

---
+++ findKey.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  let result =
+    actual === expected
+      ? `✅✅✅ Assertion Passed: [ ${actual}] === [ ${expected} ]`
+      : `🛑🛑🛑 Assertion failed: [ ${actual}  ] !== [ ${expected} ]`;
+  console.log(result);
+};
+
+const findKey = function (obj, callback) {
+  for (let [key, value] of Object.entries(obj)) {
+    if (callback(value)) return key;
+  }
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
