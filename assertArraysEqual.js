c02d7669e400b5ab3d341e41924e58911e03aa2c
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-11 01:45:16 -0500

    refactored the require into assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,6 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   let result = eqArrays(arr1, arr2)
     ? `✅✅✅ Assertion Passed: [ ${arr1}] === [ ${arr2} ]`
     : `🛑🛑🛑 Assertion failed: [ ${arr1}  ] !== [ ${arr2} ]`;
