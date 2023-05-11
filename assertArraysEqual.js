718661b2f99676281f1004229bdce2abb9e885b5
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-11 01:47:34 -0500

    expoted the lotide  assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,3 @@
-const eqArrays = require("./eqArrays");
-
 const assertArraysEqual = function(arr1, arr2) {
   let result = eqArrays(arr1, arr2)
     ? `✅✅✅ Assertion Passed: [ ${arr1}] === [ ${arr2} ]`
@@ -7,5 +5,4 @@ const assertArraysEqual = function(arr1, arr2) {
   console.log(result);
 };
 
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[1,2,4]);
+module.exports = assertArraysEqual;
