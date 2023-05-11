530a026d81f462a47b3071a10ac39379f5d46748
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-11 17:02:14 -0500

    refactored lotide middle function to use require

--- middle.js
+++ middle.js
@@ -1,21 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  let result = eqArrays(arr1, arr2)
-    ? `✅✅✅ Assertion Passed: [ ${arr1}] === [ ${arr2} ]`
-    : `🛑🛑🛑 Assertion failed: [ ${arr1}  ] !== [ ${arr2} ]`;
-  console.log(result);
-};
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function(arr1) {
   let result = [];
