92a4ef469607af1ea3133c80eef78a0f81227359
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-11 01:40:56 -0500

    refactored lotide eqArray to use module export

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = (actual, expected) => {
-  let result =
-    actual === expected
-      ? `✅✅✅ Assertion Passed: [ ${actual}] === [ ${expected} ]`
-      : `🛑🛑🛑 Assertion failed: [ ${actual}  ] !== [ ${expected} ]`;
-  console.log(result);
-};
-
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   else {
     for (let i = 0; i < arr1.length; i++) {
@@ -18,6 +10,4 @@ const eqArrays = function (arr1, arr2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), false);
-assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), false);
+module.exports = eqArrays;
