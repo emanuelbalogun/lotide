a47d5c04a0adc26dff3622657c650b830d73c70e
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-11 23:07:39 -0500

    Refactore the lotide middletest function using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,23 @@
-const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
- 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7,8]), [4,5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7,8,9]), [5]);
+const assert = require("chai").assert;
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7,8]), [4,5]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6,7,8,9]), [5]);
+
+describe("Testing out the lotide middle function", () => {
+  it("Return the middle number of the array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+
+  it("Return the middle number of the array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("Return the middle number of the array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+
+  it("Return the middle number of the array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
+  });
+});
