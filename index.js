d170f03ca7145ded33380901b1d29dada5011d43
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-12 13:19:48 -0500

    Updated the functions exported to include testing functions

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
-const letterPosition = require('./letterPositions');
-const findKey = require('./findKey')
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
-const map = require('./map');
-const without = require('./without');
-const takeUntil = require('./takeUntil');
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const letterPosition = require("./letterPositions");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const map = require("./map");
+const without = require("./without");
+const takeUntil = require("./takeUntil");
+const eqArrays = require("./eqArrays");
+const countLetter = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqObject = require("eqObject");
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  letterPosition: letterPosition,
-  findKey: findKey,
-  findKeyByValue : findKeyByValue,
-  flatten : flatten,
-  map: map,
-  without: without,
-  takeUntil: takeUntil
+  head,
+  tail,
+  middle,
+  letterPosition,
+  findKey,
+  findKeyByValue,
+  flatten,
+  map,
+  without,
+  takeUntil,
+  eqArrays,
+  countLetter,
+  countOnly,
+  eqObject
 };
