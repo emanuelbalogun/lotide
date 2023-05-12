2810a1ff1aa1e77f5763e28b8b3424b587d93f38
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-11 22:38:47 -0500

    refactored the lotide headtest using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,25 @@
 const head = require("../head");
-const assertEqual = require("../assertEqual");
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6]), 5);
-assertEqual(head([""]), 5);
+const assert = require("chai").assert;
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([6]), 5);
+// assertEqual(head([""]), 5);
+
+describe("Test for the head methods", () => {
+  it("Should return the first number", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("Should return the first word", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("Should return false", () => {
+    assert.strictEqual(head([6]), 6);
+  });
+
+  it("Should return false where array is empty", () => {
+    assert.strictEqual(head([""]), "");
+  });
+});
