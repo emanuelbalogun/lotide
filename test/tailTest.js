ed209829ce82db22616720ff56ed6e42e2b022c0
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-16 09:50:52 -0500

    implemented mochai test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,17 @@
 const tail = require("../tail.js");
-const assertEqual = require("../assertEqual.js");
+const assert = require("chai").assert;
 
 let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+
+describe("Test the tail function",()=>{
+  it("should return Lighthouse",()=>{
+    assert.equal(result[0],"Lighthouse");
+  });
+  
+  it("should return Labs",()=>{
+    assert.equal(result[1],"Labs");
+  });
+});
