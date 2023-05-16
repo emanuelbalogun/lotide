40f982d53ea9bbb15e70fb5456e3f8ed75ac3a9a
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-16 11:03:57 -0500

    refactored to check for empty array

--- flatten.js
+++ flatten.js
@@ -1,17 +1,19 @@
 //const assertArraysEqual = require('./assertArraysEqual');
 
-const flatten = function(arr1) {
+const flatten = function (arr1) {
   let result = [];
-  for (let i = 0; i < arr1.length; i++) {
-    
-    if (Array.isArray(arr1[i])) {
-      for (let ijk = 0; ijk < arr1[i].length; ijk++) {
-        result.push(arr1[i][ijk]);
+  if (Array.isArray(arr1)) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (Array.isArray(arr1[i])) {
+        for (let ijk = 0; ijk < arr1[i].length; ijk++) {
+          result.push(arr1[i][ijk]);
+        }
+      } else {
+        result.push(arr1[i]);
       }
-      
-    } else {
-      result.push(arr1[i]);
     }
+  } else {
+    return undefined;
   }
   return result;
 };
